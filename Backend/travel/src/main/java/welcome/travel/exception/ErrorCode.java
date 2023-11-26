b69ccf45089aa732d563c18ec97496561f2e7022
onlyjoon <nhj7334@icloud.com>
2023-11-26 11:06:40 +0900

    refactor - 코드 스타일 변경

--- Backend/travel/src/main/java/welcome/travel/exception/ErrorCode.java
+++ Backend/travel/src/main/java/welcome/travel/exception/ErrorCode.java
@@ -1,20 +1,16 @@
 package welcome.travel.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
     NOT_FOUND_USER(-1000, "NOT FOUND USER", HttpStatus.NOT_FOUND),
     INVALID_EMAIL(-1001, "FORBIDDEN", HttpStatus.FORBIDDEN),
     NO_COMMENTS(-1002, "NOT FOUND COMMENT", HttpStatus.NOT_FOUND),
     INVALID_TOKEN(-1003, "INVALID TOKEN", HttpStatus.FORBIDDEN);
-
-    ErrorCode(Integer code, String message, HttpStatus httpStatus) {
-        this.code = code;
-        this.message = message;
-        this.httpStatus = httpStatus;
-    }
 
     private final Integer code;
     private final String message;
